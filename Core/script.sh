d995eadd049c873dbdad7c2b7faa28dce5191fed
akshaysngupta <akshaysngupta@gmail.com>
2015-04-11 17:13:25 +0530

    Revert "scripted"
    This reverts commit 41161fb02474d67f9ece2fbdd06845bdada55de1.

--- Core/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-gnome-terminal --tab -e  "   ./Core 3 " 
-sleep 5
-gnome-terminal --tab -e "echo running   1  ./Core 2" 
-sleep 5
-gnome-terminal --tab -e  "   ./Core 1" 
-sleep 5
-gnome-terminal --tab -e  "   ./Core 0"
